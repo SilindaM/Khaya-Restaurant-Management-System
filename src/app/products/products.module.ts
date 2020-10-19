eead3b2a275aa872153b636879559bcf4b9ff3ee
SilindaM <silindanemdu@gmail.com>
2020-10-19 17:03:47 +0200

    product module

--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -2,13 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ProductsRoutingModule } from './products-routing.module';
+import { AddProductComponent } from './add-product/add-product.component';
+import { ViewProductComponent } from './view-product/view-product.component';
+import { UpdateProductComponent } from './update-product/update-product.component';
+import { DeleteProductComponent } from './delete-product/delete-product.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [AddProductComponent, ViewProductComponent, UpdateProductComponent, DeleteProductComponent],
   imports: [
     CommonModule,
     ProductsRoutingModule
-  ]
+  ],
+  exports :[AddProductComponent, ViewProductComponent, UpdateProductComponent, DeleteProductComponent]
 })
 export class ProductsModule { }
