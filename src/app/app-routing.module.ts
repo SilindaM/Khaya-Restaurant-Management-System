525a51ffa75d41a0fc41dd31da740cfb430fbf89
SilindaM <silindanemdu@gmail.com>
2020-10-19 16:44:57 +0200

    commit modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'orders',loadChildren:()=>import('./orders/orders.module').then(m=>m.OrdersModule)},
+  {path:'customers',loadChildren:()=>import('./customers/customers.module').then(m=>m.CustomersModule)},
+  {path:'bills',loadChildren:()=>import('./bills/bills.module').then(m=>m.BillsModule)},
+  {path:'products',loadChildren:()=>import('./products/products.module').then(m=>m.ProductsModule)},
+  {path:'staff',loadChildren:()=>import('./staff/staff.module').then(m=>m.StaffModule)},
+  {path:'tables',loadChildren:()=>import('./tables/tables.module').then(m=>m.TablesModule)},
+  {path:'menu',loadChildren:()=>import('./menu/menu.module').then(m=>m.MenuModule)}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
