c3636df6ccf8645c4d79ef6d9d56615fd8c23d44
SilindaM <silindanemdu@gmail.com>
2020-10-22 00:19:22 +0200

    AddTableComponentCreation

--- src/app/tables/tables-routing.module.ts
+++ src/app/tables/tables-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AddtableComponent } from './addtable/addtable.component';
+import { ViewtablesComponent } from './viewtables/viewtables.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'addtable',component:AddtableComponent},
+  {path:'viewtables',component:ViewtablesComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/tables/tables.module.ts
+++ src/app/tables/tables.module.ts
@@ -2,13 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { TablesRoutingModule } from './tables-routing.module';
+import { AddtableComponent } from './addtable/addtable.component';
+import { ViewtablesComponent } from './viewtables/viewtables.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [AddtableComponent, ViewtablesComponent],
   imports: [
     CommonModule,
     TablesRoutingModule
-  ]
+  ],
+  exports:[AddtableComponent, ViewtablesComponent]
 })
 export class TablesModule { }
