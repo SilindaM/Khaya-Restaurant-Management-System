549b78481468ed8e8c95adc6d3c2da23c967af3e
SilindaM <silindanemdu@gmail.com>
2020-10-22 00:18:21 +0200

    viewTableComponentCreation

---
+++ src/app/tables/addtable/addtable.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-addtable',
+  templateUrl: './addtable.component.html',
+  styleUrls: ['./addtable.component.css']
+})
+export class AddtableComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/tables/viewtables/viewtables.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-viewtables',
+  templateUrl: './viewtables.component.html',
+  styleUrls: ['./viewtables.component.css']
+})
+export class ViewtablesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
