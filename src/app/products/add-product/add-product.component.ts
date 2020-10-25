f8ae527a392f60c2bc2b1c45ff63119da20c414c
SilindaM <silindanemdu@gmail.com>
2020-10-25 21:52:54 +0200

    add-product modifications

--- src/app/products/add-product/add-product.component.ts
+++ src/app/products/add-product/add-product.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add-product',
@@ -7,9 +8,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddProductComponent implements OnInit {
 
-  constructor() { }
+  isSubmitted=false;
+  productRegistration:FormGroup
+  constructor(private fb:FormBuilder) { }
 
-  ngOnInit(): void {
+  AddNewProductForm(){
+    this.productRegistration=this.fb.group({
+      pName:['',Validators.required],
+      pPrice:['',Validators.required],
+      pQuantity:['',Validators.required],
+      pImage:['',Validators.required],
+      pDescription:['',Validators.required],
+      pCategory:['',Validators.required]
+    })
   }
-
+  get NameValidator(){
+    return this.productRegistration.get('pName') as FormControl;
+  }
+  get PriceValidator(){
+    return  this.productRegistration.get('pPrice')  as FormControl;
+  }
+  get QuantityValidator(){
+    return  this.productRegistration.get('pQuantity') as FormControl;
+  }
+  get ImageValidator(){
+    return  this.productRegistration.get('pImage')  as FormControl;
+    
+  }
+  get DescriptionValidator(){
+    return  this.productRegistration.get('pDescription')  as FormControl;
+  }
+  get CategoryValidator(){
+    return  this.productRegistration.get('pCategory') as FormControl;
+  }
+  
+  onSubmit(){
+    this.isSubmitted=true;
+    if(!this.productRegistration.valid){
+      return  false;
+    }
+    else{
+      alert(JSON.stringify(this.productRegistration.value))
+    }
+  }
+  ngOnInit(): void {
+    this.AddNewProductForm();
+  }
+  
 }
