6786e3a8aeaa6ff83f3c2234015b87cbe2775715
SilindaM <silindanemdu@gmail.com>
2020-10-26 12:47:13 +0200

    add content cmponent

---
+++ src/app/mainpage/content/content.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { Products } from 'src/app/products/product';
+import { ProductsService } from 'src/app/products/products.service';
+
+@Component({
+  selector: 'app-content',
+  templateUrl: './content.component.html',
+  styleUrls: ['./content.component.css']
+})
+export class ContentComponent implements OnInit {
+
+ 
+
+producta:Products;
+  constructor(private services:ProductsService) { }
+
+  ngOnInit(): void {
+    this.getProducts();
+  }
+  getProducts(){
+    this.services.getProducts().subscribe(data=>{
+      this.producta=data;
+    })
+  }
+}
